2637495e76ee264e29d21b78a5d6e56b619c98c3
jayasurya <jayasurya@sparkouttech.com>
2025-08-01 18:34:13 +0530

    feat: login & register api created, prisma & prisma client installed - to configure database, & in the db user table has been created

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
+import bcrypt from 'bcryptjs';
+
+export async function POST(req: NextRequest) {
+    
+    try {
+        const body = await req.json();
+        const { email, password } = body;
+
+        if (!email || !password) {
+            return NextResponse.json(
+                { error: "Email Id and Password should be required" },
+                {status: 400} 
+            );
+        }
+
+        const user = await prisma.user.findUnique({
+            where:{ email }
+        });
+
+        if(!user) {
+            return NextResponse.json(
+                { error: "User not found" },
+                { status: 404 }
+            )
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return NextResponse.json(
+                { error: "Invalid credentials" },
+                { status: 401 }
+            )
+        };
+
+        return NextResponse.json(
+            { message: "login successfully" },
+            { status: 200 }
+        )
+
+    } catch (error) {
+        return NextResponse.json(
+            {error: "Intrnal Server Error"},
+            { status: 500 }
+        )
+    }
+
+}
---
+++ src/app/api/register/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "../../../../lib/prisma";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: NextRequest) {
+    
+    try {
+
+        const body = await req.json();
+        const { name, email, password } = body;
+
+        if (!name || !email || !password) {
+            return NextResponse.json(
+                { error: "Name, Email Id and Password should be required" },
+                {status: 400} 
+            );
+        }
+
+        const existingUser = await prisma.user.findUnique({
+            where:{ email }
+        });
+
+        if(existingUser) {
+            return NextResponse.json(
+                { error: "User already exists" },
+                { status: 409 }
+            )
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password
+            }
+        })
+
+        return NextResponse.json(
+            { message: "User created successfully", user: newUser },
+            { status: 200 }
+        )
+
+    } catch (error) {
+        return NextResponse.json(
+            {error: "Intrnal Server Error"},
+            { status: 500 }
+        )
+    }
+
+}
